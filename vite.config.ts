f2cdd10b324d70418cb12554a827a2af1416caaf
jdmanabat <manabat.johnder@gmail.com>
2022-12-06 21:09:41 +0800

    add eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+// import eslint from 'vite-plugin-eslint';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  server: {
+    host: true,
+    port: 3000,
+  },
+  plugins: [react(), tsconfigPaths()],
+});
